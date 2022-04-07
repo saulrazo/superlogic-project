bec4289a14bb0f74377419335eb712cf6fc7bf49
saulrazo <saulrazom32@gmail.com>
2022-04-07 17:53:03 -0500

    1

--- superlogic/calculator/views.py
+++ superlogic/calculator/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request): #PÁGINA INICIAL
+    return render(request, 'index.html')
+
+
+
+def sets(request): #PÁGINA DE CONJUNTOS
+
+    if request.GET:
+       set1 = (list(request.GET.values()))[0]
+       set2 = (list(request.GET.values()))[1]
+       option = (list(request.GET.values()))[2]
+       context = {'set1':set1,'set2':set2,'option':option}
+
+       return render(request, 'sets.html', context)
+
+    else:
+       return render(request, 'sets.html')
+
+
+
+
+
--- superlogic/superlogic/settings.py
+++ superlogic/superlogic/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'calculadora.apps.CalculatorConfig',
+    'calculator.apps.CalculatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
