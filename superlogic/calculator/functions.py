1dc90e25ef7fac020a7893f54f344b5a8c156907
saulrazo <saulrazom32@gmail.com>
2022-04-19 18:44:55 -0500

    1

--- superlogic/calculator/functions.py
+++ superlogic/calculator/functions.py
@@ -1,3 +1,29 @@
+import random
+
+def auto_Generate():
+    size_A = random.randint(1, 20)
+    size_B = random.randint(1, 20)
+    conj_A = []
+    conj_B = []
+    for i in range(size_A):
+        rep = 0
+        while rep == 0:
+            rand = random.randint(1, 99)
+            if rand not in conj_A:
+                conj_A.append(rand)
+                rep = 1
+
+    for i in range(size_B):
+        rep = 0
+        while rep == 0:
+            rand = random.randint(1, 99)
+            if rand not in conj_B:
+                conj_B.append(rand)
+                rep = 1
+    return conj_A, conj_B
+
+
+
 def intersection(conj_A, conj_B):
     card_A = int(len(conj_A))
     card_B = int(len(conj_B))
--- superlogic/calculator/views.py
+++ superlogic/calculator/views.py
@@ -12,31 +12,64 @@ def sets(request): #PÁGINA DE CONJUNTOS
     if request.GET:
        set1 = list(set(((list(request.GET.values()))[0]).split(sep=',')))
        set2 = list(set(((list(request.GET.values()))[1]).split(sep=',')))
-       option = int((list(request.GET.values()))[2])
+       random_option = int((list(request.GET.values()))[2])
+       option = int((list(request.GET.values()))[3])
 
-       if option == 1:
-           oper_list = intersection(set1, set2)
-           if oper_list:
-                response = intersection(set1, set2)
-           else:
-                response = "No hay interseccion!"
+       if random_option == 1:
 
-           context = {'set1':set1,'set2':set2,'option':option,'result':response}
-           return render(request, 'sets.html', context)
+          if option == 1:
+              oper_list = intersection(set1, set2)
+              if oper_list:
+                   response = intersection(set1, set2)
+              else:
+                   response = "No hay interseccion!"
 
-       elif option == 2:
-           oper_list = union(set1, set2)
-           response = union(set1, set2)
+              context = {'set1':set1,'set2':set2,'option':option,'result':response}
+              return render(request, 'sets.html', context)
+
+          elif option == 2:
+              oper_list = union(set1, set2)
+              response = union(set1, set2)
 
     
-           context = {'set1':set1,'set2':set2,'option':option,'result':response}
-           return render(request, 'sets.html', context)
+              context = {'set1':set1,'set2':set2,'option':option,'result':response}
+              return render(request, 'sets.html', context)
 
          
 
-       else:
-           context = {'set1':set1,'set2':set2,'option':option}
-           return render(request, 'sets.html', context)
+          else:
+              context = {'set1':set1,'set2':set2,'option':option}
+              return render(request, 'sets.html', context)
+
+       elif random_option == 2:
+            set1, set2 = auto_Generate()
+
+            if option == 1:
+                oper_list = intersection(set1, set2)
+                if oper_list:
+                     response = intersection(set1, set2)
+                else:
+                     response = "No hay interseccion!"
+
+                context = {'set1':set1,'set2':set2,'option':option,'result':response}
+                return render(request, 'sets.html', context)
+
+            elif option == 2:
+                oper_list = union(set1, set2)
+                response = union(set1, set2)
+
+    
+                context = {'set1':set1,'set2':set2,'option':option,'result':response}
+                return render(request, 'sets.html', context)
+
+         
+
+            else:
+                context = {'set1':set1,'set2':set2,'option':option}
+                return render(request, 'sets.html', context)
+
+
+
 
 
     else:
