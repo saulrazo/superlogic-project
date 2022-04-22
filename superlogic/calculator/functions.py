a0eff5349a2595de8c3c754198658384b2c73b17
saulrazo <saulrazom32@gmail.com>
2022-04-21 19:43:39 -0500

    1

--- superlogic/calculator/functions.py
+++ superlogic/calculator/functions.py
@@ -39,13 +39,13 @@ def menu(set1,set2,option):
         context = {'set1':set1,'set2':set2,'option':option,'result':response}
         return context
 
-    elif option == 6: #COMPLEMENTO DE A ///
+    elif option == 6: #COMPLEMENTO DE A
         response = complement(set1)
 
         context = {'set1':set1,'set2':set2,'option':option,'result':response}
         return context
 
-    elif option == 7: # COMPLEMENTO DE B ///
+    elif option == 7: # COMPLEMENTO DE B 
         oper_list = union(set1, set2)
         response = union(set1, set2)
 
@@ -148,10 +148,10 @@ def difference(conj_A, conj_B):
 
 def complement(conj):
     card = int(len(conj))
-    universe = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
-                55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+    universe = []
+    for element in range(100):
+        universe.append(str(element+1))
+
     comp = universe.copy()
 
     for i in range(card):
